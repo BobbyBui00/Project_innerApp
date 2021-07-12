489f723f654a709f98b5e3ebe8833a3e1f508877
BobbyBui <kelvinkhoabui@gmail.com>
2021-07-12 11:36:46 -0400

    Initial commit

---
+++ src/main/java/Chris/Allain/project/innerApp/InnerAppApplication.java
@@ -0,0 +1,13 @@
+package Chris.Allain.project.innerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Chris/Allain/project/innerApp/InnerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Chris.Allain.project.innerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
